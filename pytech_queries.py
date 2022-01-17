43cb9810705879b3a5526d92fdb82829297901ed
MalcolmXtreme <70729560+MalcolmXtreme@users.noreply.github.com>
2022-01-16 19:40:00 -0500

    Add files via upload

---
+++ pytech_queries.py
@@ -0,0 +1,28 @@
+import collections
+from xml.dom.minidom import Document
+import pymongo
+
+from pymongo import MongoClient
+
+client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.kpnte.mongodb.net/Pytech")
+db = client.Pytech
+
+
+
+db.students.find({Document})
+
+
+
+Document = collections.find({})
+
+    
+                      
+   
+  
+
+
+
+
+
+
+
